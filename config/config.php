143eebf52095e885f15495cd299121c611031852
rohitjoey <roht.praz@gmail.com>
2020-05-21 14:59:11 +0545

    config folder added

---
+++ config/config.php
@@ -0,0 +1,18 @@
+<?php
+	ob_start();
+	session_start();
+	date_default_timezone_set('Asia/Kathmandu');
+
+?>		
+
+
+
+
+
+<!-- <Directory "C:/xampp/htdocs/project_magazine/">
+        Options Indexes FollowSymLinks MultiViews
+        AllowOverride all
+        Order Deny,Allow
+        Allow from all
+        Require all granted
+</Directory> -->
---
+++ config/function.php
@@ -0,0 +1,31 @@
+ <?php
+	function debugger($data,$is_die=false){
+		echo "<pre>";
+		print_r($data);
+		echo "</pre>";
+		if($is_die){
+			exit();
+		}
+	} 
+
+	function sanitize($str){
+		return trim(stripcslashes(strip_tags(str)));
+
+	}
+
+	function tokenize($length=100){
+		$char='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$token='';
+		for ($i=0; $i < $length; $i++) { 
+				$token.=$char[rand(0,$len-1)];
+		}
+		retun $token;	
+	}
+
+	function redirect($loc,$key,$message){
+		$_SESSION[$key]=$message;
+		@header('location: '.$loc);
+	}
+
+
+?>
